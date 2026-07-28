3aa6267662864c2a16ac0511eff5a6eed843fa5b
missurbhi <surbhiver073@gmail.com>
2026-07-28 16:35:07 +0530

    initial commit

--- OneDrive/Desktop/Adv java/Git/Deploy.java
+++ OneDrive/Desktop/Adv java/Git/Deploy.java
@@ -4,5 +4,6 @@ public class Deploy {
     public static void main(String[] args) {
         System.out.println("Deploying application...");
         System.out.println("Making Changess");
+        System.out.println("Making Changes 2");
     }
 }
